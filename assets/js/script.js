b0b553a0fca89494d65e63de336385d53daf20e0
EmillyWeber <omockingbird196@gmail.com>
2025-07-05 19:20:06 -0400

    Alterações para a página

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,5 +28,30 @@
     }
   }
 
+  function openVideo(button) {
+    // Pega o caminho do vídeo do atributo data-video
+    var videoPath = button.getAttribute('data-video');
+    
+    // Exibe o modal com o vídeo
+    var modal = document.getElementById("videoModal");
+    var videoFrame = document.getElementById("videoFrame");
+    
+    // Define o src do iframe com o caminho do vídeo
+    videoFrame.src = videoPath;  // Agora o caminho será dinâmico
+
+    modal.style.display = "flex";
+}
+
+function closeVideo(event) {
+    var modal = document.getElementById("videoModal");
+
+    // Se o clique for fora do vídeo, fecha o modal
+    if (event.target === modal) {
+        var videoFrame = document.getElementById("videoFrame");
+        videoFrame.src = "";  // Para o vídeo ao fechar
+        modal.style.display = "none";
+    }
+}
+
   window.addEventListener('load', trocarH2porH3);
   window.addEventListener('resize', trocarH2porH3);
